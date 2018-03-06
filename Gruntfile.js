ff41a43f43463c0d5be0f0f8253cd2c0548d079e
Andy Liebke <coding@andysmiles4games.com>
2018-03-07 00:09:38 +0100

    Add SASS task to the grunt configuration file
    [Close #5]

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,21 @@
 module.exports = function(grunt)
 {
+    grunt.initConfig({
+        sass: {
+            options: {
+                style: 'compressed',
+                sourcemap: 'none'
+            },
 
+            dist: {
+                files: {
+                    'Dist/FullscreenTheme/Assets/Stylesheet/Theme.min.css': 'Src/FullscreenTheme/Assets/Stylesheet/Theme.scss'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    grunt.registerTask('default', ['sass']);
 };
